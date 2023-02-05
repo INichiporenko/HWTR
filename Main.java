7fa2eb5b3d7d2252a25d5504dcb6d4ebcf0b740a
INichiporenko <123013119+INichiporenko@users.noreply.github.com>
2023-02-05 15:18:31 +0100

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,4 +1,23 @@
-public class Main {
-    public static void main(String[] args) {System.out.println ("Iryna Nychyporenko");}
+import java.util.Locale;
+import java.util.Scanner;
 
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ведіть аргумент 1");
+        int arg1  = scanner.nextInt();
+        System.out.println("Ведіть аргумент 2");
+        int arg2 = scanner.nextInt();
+
+        int result = arg1 + arg2;
+
+        System.out.println(Integer.toBinaryString(arg1) + " + " + Integer.toBinaryString(arg2) + " = " + Integer.toBinaryString(result));
+
+
+
+
+
+
+    }
 }
